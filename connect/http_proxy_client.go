acd2970ccba8da81227c0264eccbf13829e9a7a6
masx200 <masx200@qq.com>
2025-09-12 04:55:08 +0800

    删除无用结构体定义

--- connect/http_proxy_client.go
+++ connect/http_proxy_client.go
@@ -6,43 +6,20 @@ import (
 	"net"
 	"net/url"
 	"strconv"
-	"time"
+
+	"github.com/masx200/socks5-websocket-proxy-golang/pkg/interfaces"
 )
-
-// CreateClientFunc 客户端创建函数类型
-type CreateClientFunc func(config ClientConfig) (ProxyClient, error)
-
-// ProxyClient 代理客户端接口
-type ProxyClient interface {
-	Connect(host string, port int) error
-	// Authenticate(username, password string) error
-	ForwardData(conn net.Conn) error
-	Close() error
-	SetConnectionClosedCallback(callback func()) error
-	NetConn() net.Conn
-
-	DialContext(ctx context.Context, network, addr string) (net.Conn, error)
-}
-
-// ClientConfig 客户端配置
-type ClientConfig struct {
-	Username   string        `json:"username"`    // 用户名
-	Password   string        `json:"password"`    // 密码
-	ServerAddr string        `json:"server_addr"` // 服务器地址
-	Protocol   string        `json:"protocol"`    // 协议类型
-	Timeout    time.Duration `json:"timeout"`     // 超时时间
-}
 
 // HttpProxyClient HTTP代理客户端实现
 type HttpProxyClient struct {
-	config        ClientConfig
+	config        interfaces.ClientConfig
 	conn          net.Conn
 	closed        bool
 	closeCallback func()
 }
 
 // NewHttpProxyClient 创建HTTP代理客户端
-func NewHttpProxyClient(config ClientConfig) (ProxyClient, error) {
+func NewHttpProxyClient(config interfaces.ClientConfig) (interfaces.ProxyClient, error) {
 	return &HttpProxyClient{
 		config: config,
 		closed: false,
@@ -182,6 +159,6 @@ func (c *HttpProxyClient) DialContext(ctx context.Context, network, addr string)
 }
 
 // CreateHttpProxyClient 创建HTTP代理客户端的工厂函数
-var CreateHttpProxyClient CreateClientFunc = func(config ClientConfig) (ProxyClient, error) {
+var CreateHttpProxyClient interfaces.CreateClientFunc = func(config interfaces.ClientConfig) (interfaces.ProxyClient, error) {
 	return NewHttpProxyClient(config)
 }
