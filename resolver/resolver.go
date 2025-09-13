d9ff9c89ff99faadcdac08ea52f1afdc62a77a58
masx200 <masx200@qq.com>
2025-09-13 13:48:07 +0100

    feat(resolver): 添加多个解析器实现
    添加新的DNS解析器类型，包括HostsAndDohResolver、DOHResolver、
    DOH3Resolver和HostsResolver。每个解析器都实现了NameResolver
    接口，目前为占位符实现。同时添加创建这些解析器实例的工厂函数。

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -3,9 +3,75 @@ package resolver
 import (
 	"context"
 	"net"
+	"net/http"
+
+	"github.com/masx200/http-proxy-go-server/options"
 )
 
 type NameResolver interface {
 	Resolve(ctx context.Context, name string) (context.Context, net.IP, error)
 	LookupIP(ctx context.Context, network, host string) ([]net.IP, error)
+}
+type HostsAndDohResolver struct{}
+
+// LookupIP implements NameResolver.
+func (h *HostsAndDohResolver) LookupIP(ctx context.Context, network string, host string) ([]net.IP, error) {
+	panic("unimplemented")
+}
+
+// Resolve implements NameResolver.
+func (h *HostsAndDohResolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
+	panic("unimplemented")
+}
+
+func CreateHostsAndDohResolver(proxyoptions options.ProxyOptions, tranportConfigurations ...func(*http.Transport) *http.Transport) NameResolver {
+	return &HostsAndDohResolver{}
+}
+func CreateDOHResolver(proxyoptions options.ProxyOptions, tranportConfigurations ...func(*http.Transport) *http.Transport) NameResolver {
+	return &DOHResolver{}
+}
+
+type DOHResolver struct{}
+
+// LookupIP implements NameResolver.
+func (d *DOHResolver) LookupIP(ctx context.Context, network string, host string) ([]net.IP, error) {
+	panic("unimplemented")
+}
+
+// Resolve implements NameResolver.
+func (d *DOHResolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
+	panic("unimplemented")
+}
+
+func CreateDOH3Resolver(proxyoptions options.ProxyOptions) NameResolver {
+	return &DOH3Resolver{}
+}
+
+type DOH3Resolver struct{}
+
+// LookupIP implements NameResolver.
+func (d *DOH3Resolver) LookupIP(ctx context.Context, network string, host string) ([]net.IP, error) {
+	panic("unimplemented")
+}
+
+// Resolve implements NameResolver.
+func (d *DOH3Resolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
+	panic("unimplemented")
+}
+
+func CreateHostsResolver() NameResolver {
+	return &HostsResolver{}
+}
+
+type HostsResolver struct {
+}
+
+// LookupIP implements NameResolver.
+func (h *HostsResolver) LookupIP(ctx context.Context, network string, host string) ([]net.IP, error) {
+	panic("unimplemented")
+}
+
+// Resolve implements NameResolver.
+func (h *HostsResolver) Resolve(ctx context.Context, name string) (context.Context, net.IP, error) {
+	panic("unimplemented")
 }
