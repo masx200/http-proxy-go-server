ba55d2155b47dae886d70a9eaf688ba883199916
masx200 <masx200@qq.com>
2025-12-01 17:57:26 +0000

    Update tls.go

--- tls/tls.go
+++ tls/tls.go
@@ -31,7 +31,7 @@ func Tls(server_cert string, server_key, hostname string, port int, proxyoptions
 	xh := http_server.GenerateRandomLoopbackIP()
 	x1 := http_server.GenerateRandomIntPort()
 	var upstreamAddress string = xh + ":" + fmt.Sprint(rune(x1))
-	go http_server.Http(xh, x1, proxyoptions, dnsCache, "", "", tranportConfigurations...)
+	go http_server.Http(xh, x1, proxyoptions, dnsCache, "", "", upstreamResolveIPs, tranportConfigurations...)
 	// 死循环，每当遇到连接时，调用 handle
 	for {
 		client, err := l.Accept()
