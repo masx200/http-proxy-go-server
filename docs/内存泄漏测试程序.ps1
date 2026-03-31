d2871228a2b9871d1d7f80c698945c5d56efea49
masx200 <masx200@qq.com>
2026-03-31 11:52:28 +0800

    Update 内存泄漏测试程序.ps1

--- docs/内存泄漏测试程序.ps1
+++ docs/内存泄漏测试程序.ps1
@@ -8,14 +8,14 @@ go run ./cmd/ -enable-pprof  '-dohurl' 'https://doh-server.masx200.ddns-ip.net' 
 
 for (; ; ) {
 
-    curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://one.one.one.one -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query --connect-timeout 10 --max-time 10
+    curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dash.cloudflare.com/ -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query --connect-timeout 10 --max-time 10
     
     sleep 2
     curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv4.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
     sleep 5
     curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ifconfig.co/json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
     sleep 5
-    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dns.google -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10;
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://note.wps.cn/ -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10;
 
 
 
