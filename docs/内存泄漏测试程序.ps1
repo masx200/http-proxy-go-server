a9bf88a8d1df8c611f062ad79ee21be44855df32
masx200 <masx200@qq.com>
2026-03-31 11:26:25 +0800

    Create 内存泄漏测试程序.ps1

---
+++ docs/内存泄漏测试程序.ps1
@@ -0,0 +1,23 @@
+go run ./cmd/ -enable-pprof  '-dohurl' 'https://doh-server.masx200.ddns-ip.net' '-dohip' '104.21.14.41' '--dohalpn=h2' '-port' '28340' '-username' 'admin' '-password' 'iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58' '-dohalpn=h3' '-dohurl' 'https://doh-server.masx200.ddns-ip.net' '--dohip=104.21.9.230' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h3' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h3' -cache-file ./data/dns_cache.json -cache-aof-file ./data/dns_cache.aof  --ip-priority ipv6
+
+
+
+
+
+
+
+for (; ; ) {
+
+    curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://one.one.one.one -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query
+    
+    sleep 2
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv4.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query
+    sleep 5
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ifconfig.co/json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query
+    sleep 5
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dns.google -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query;
+
+
+
+    sleep 5;
+}
