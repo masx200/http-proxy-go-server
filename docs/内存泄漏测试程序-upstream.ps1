9c79f3a1ca046f497c4992a27387f21eaa43cb78
masx200 <masx200@qq.com>
2026-04-30 17:08:33 +0800

    Update 内存泄漏测试程序-upstream.ps1

--- docs/内存泄漏测试程序-upstream.ps1
+++ docs/内存泄漏测试程序-upstream.ps1
@@ -13,7 +13,8 @@ go run ./cmd/ -enable-pprof  '-dohurl' 'https://61919494499.security.cloudflare-
 for (; ; ) {
 
     curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dash.cloudflare.com/ -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query --connect-timeout 10 --max-time 10
-    
+     sleep 2
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv6.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
     sleep 2
     curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv4.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
     sleep 5
