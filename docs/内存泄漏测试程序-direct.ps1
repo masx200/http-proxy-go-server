7d4a01870c64f10077bc0e03400f75fc89b76259
masx200 <masx200@qq.com>
2026-05-05 14:10:34 +0800

    ``` feat(docs): 更新内存泄漏测试程序中的DNS查询配置
    将所有测试脚本中的DNS查询URL从OpenDNS更改为Cloudflare Gateway，
    以改善DNS解析性能和隐私保护。同时新增了一个专门的DNS测试脚本，
    用于测试不同DNS配置下的内存泄漏情况。
    
    BREAKING CHANGE: DNS解析服务从OpenDNS切换到Cloudflare Gateway
    ```

--- docs/内存泄漏测试程序-direct.ps1
+++ docs/内存泄漏测试程序-direct.ps1
@@ -12,14 +12,14 @@ go run ./cmd/ -enable-pprof  '-dohurl' 'https://61919494499.security.cloudflare-
 
 for (; ; ) {
 
-    curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dash.cloudflare.com/ -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query --connect-timeout 10 --max-time 10
+    curl  --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://dash.cloudflare.com/ -L -x http://127.0.0.1:28340 --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query --connect-timeout 10 --max-time 10
     
     Start-Sleep 2
-    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv4.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ipv4.ipleak.net/?mode=json -L -x http://127.0.0.1:28340 --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query  --connect-timeout 10 --max-time 10
     Start-Sleep 5
-    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ifconfig.co/json -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v  https://ifconfig.co/json -L -x http://127.0.0.1:28340 --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query  --connect-timeout 10 --max-time 10
     Start-Sleep 5
-    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://note.wps.cn/ -L -x http://127.0.0.1:28340 --doh-url https://doh.opendns.com/dns-query  --connect-timeout 10 --max-time 10;
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I https://note.wps.cn/ -L -x http://127.0.0.1:28340 --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query  --connect-timeout 10 --max-time 10;
 
 
 
---
+++ docs/内存泄漏测试程序-dns.ps1
@@ -0,0 +1,27 @@
+
+
+
+
+go run ./cmd/ -enable-pprof  '-dohurl' 'https://61919494499.security.cloudflare-dns.com/dns-query' '-dohip' '104.21.14.41' '--dohalpn=h2' '-port' '28340' '-username' 'admin' '-password' 'iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58' '-dohalpn=h3' '-dohurl' 'https://61919494499.security.cloudflare-dns.com/dns-query' '--dohip=104.21.9.230' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h2' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h3' '-dohurl' 'https://pngwczx94z.cloudflare-gateway.com/dns-query' '-dohip' '104.21.9.230' '--dohalpn=h3' -cache-file ./data/dns_cache.json -cache-aof-file ./data/dns_cache.aof  --ip-priority ipv6 "-upstream-address" "socks5://127.0.0.1:20808" "-upstream-type" "socks5" "--upstream-resolve-ips=true"  --upstream-username "hp40emnw108got6a67p2isj1x65qwjtz60fh5dtl7nhjhor3va" --upstream-password "i7esr1nwxcil034gslw4sdzjyfejfvf5xiaagx4x286nw6l3ff" 
+
+
+
+
+
+
+# 定义生成随机英文字符串的函数，默认长度6位
+function Get-RandomAlpha($length=6){
+    $chars = 65..90 + 97..122 | ForEach-Object { [char]$_ }
+    -join ($chars | Get-Random -Count $length)
+}
+
+for () {
+    # 生成随机前缀
+    $prefix = Get-RandomAlpha
+    # 拼接目标URL
+    $targetUrl = "https://${prefix}.dns4.browserleaks.org/"
+    
+    curl --proxy-user admin:iy3w0rqwftfb1z7jr2nd4c894rc8t3pxhtw1qj94bxnjvioq58 -v -I $targetUrl -L -x http://127.0.0.1:28340 --doh-url https://pngwczx94z.cloudflare-gateway.com/dns-query  --connect-timeout 10 --max-time 10
+
+    Start-Sleep 5
+}
